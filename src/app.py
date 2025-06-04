c6ece690bdc8b94cd68a80f1d533dc072b714569
mineraleee <helenigratia@gmail.com>
2025-06-04 18:49:32 +0700

    feat: add app.py

---
+++ src/app.py
@@ -0,0 +1,8 @@
+# app.py
+from gui.components.main_window import MainWindow
+import customtkinter as ctk
+
+if __name__ == "__main__":
+    ctk.set_appearance_mode("dark")
+    app = MainWindow()
+    app.mainloop()
